3a171694b80b6354cf74866c4c3f15451bf3fb7b
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2023-03-11 17:04:36 +0200

    create NavBar component. Create notFound component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
           <Routes>
               <Route path='/' element={<Home/>}/>
               <Route path='/cart' element={<Cart/>}/>
+              <Route path='not-found' element={<NotFound/>}/>
+              <Route path='*' element={<NotFound/>}/>
           </Routes>
         </BrowserRouter>
     </div>
