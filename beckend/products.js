0788b03d94daf82b539417aee704a6aa3575deec
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2023-03-12 19:43:28 +0200

    create reducer

---
+++ beckend/products.js
@@ -0,0 +1,26 @@
+
+const products = [
+    {
+        id: 1,
+        name: 'iPhone 12 pro',
+        description: '6.1-inch display',
+        price: 999,
+        image: 'https://res.cloudinary.com/dw7scy18o/image/upload/v1673380668/onlineShop/iphone12pro_uymfbx.jpg',
+    },
+    {
+        id: 2,
+        name: 'iPhone 12',
+        description: '5.1-inch display',
+        price: 699,
+        image: 'https://res.cloudinary.com/dw7scy18o/image/upload/v1673380639/onlineShop/iphone12_lnelnm.jpg',
+    },
+    {
+        id: 3,
+        name: 'Galaxy S',
+        description: '6.5-inch display',
+        price: 399,
+        image: 'https://res.cloudinary.com/dw7scy18o/image/upload/v1673380628/onlineShop/galaxyS_hhm2ly.png',
+    },
+];
+
+module.exports = products;
