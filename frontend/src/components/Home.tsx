ffaf6d120733dc20e87a3995de55a585983ab47f
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2023-03-20 19:29:08 +0200

    styled Home component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import {useGetAllProductsQuery} from "../features/productsApi";
+import {Products} from "../features/productsSlice";
 
 const Home = () => {
-    // @ts-ignore
-    const { data, error, isLoading} = useGetAllProductsQuery();
-    return (
-        <div>
+    const { data, error, isLoading} = useGetAllProductsQuery('');
 
+
+    return (
+        <div className='home-container'>
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>An error occurred..</p>
+            ) : (
+                <>
+                    <h2>New Arrivals</h2>
+
+                    <div className="products">
+                        {
+                           Array.isArray(data) && data?.map((product: Products) => (
+                               <div
+                                key={product.id}
+                                className={'product'}
+                               >
+                            <h3>{product.name}</h3>
+                            <img
+                                src={product.image}
+                                alt={product.name}
+                            />
+                            <div className='details'>
+                                <span>{product.description}</span>
+                                <span className='price'>${product.price}</span>
+                            </div>
+                            <button>Add To Cart</button>
+                        </div>
+                        ))}
+                    </div>
+                </>
+            )}
         </div>
     );
 };
--- frontend/src/features/productsApi.ts
+++ frontend/src/features/productsApi.ts
@@ -2,13 +2,15 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import {Products} from "./productsSlice";
 
 export interface ProductsResponse {
-    items: Products [],
+    data: Products [],
 }
 
 
 export const productsApi = createApi({
     reducerPath: 'productsApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:5000'
+    }),
     endpoints: (build) => ({
         getAllProducts: build.query<ProductsResponse, string>({
             query: () => 'products',
