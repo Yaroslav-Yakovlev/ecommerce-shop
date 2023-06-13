cda67a66e1ce336b76e86b3b47b32c208c4fd814
Yaroslav Yakovlev <yakovlevyaroslav1@gmail.com>
2023-06-13 21:43:06 +0300

    changed cart component, added styled components

---
+++ frontend/src/components/styles/CartContainer.styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CartContainerStyled = styled.div`
+  margin: 2rem 0 1rem 0;
+  padding: 2rem 4rem;
+
+  h2 {
+    font-weight: 400;
+    font-size: 30px;
+    text-align: center;
+  }
+`;
